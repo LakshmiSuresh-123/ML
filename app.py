6b6bbc5859a3c300f22479dcfb1e4e19b8a8a2d6
Lakshmi <lakshmisuresh2810@gmail.com>
2023-05-17 12:19:00 +0530

    fifth commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+st.title('Medical Diagnostic Prediction')
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 
